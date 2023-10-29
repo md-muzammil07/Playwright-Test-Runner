7b9a4e9141a9b06237e243cd06cb1ff701d017ba
muzamil <muzamil.md@ibexlabs.com>
2023-10-29 11:18:04 +0530

    install

---
+++ playwright.config.ts
@@ -0,0 +1,22 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+
+const config: PlaywrightTestConfig = {
+
+    use: {
+        headless: false,
+        channel: "chrome",
+        screenshot:"only-on-failure",
+        video: "retry-with-video",
+        launchOptions: {
+            slowMo: 500
+        }
+
+    },
+    testMatch:["youTube.test.ts"],
+    retries: 0,
+    reporter: [['html', { open: 'never' }]],
+    
+}
+
+export default config;
